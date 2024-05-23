a92f8718435e526b2919f8dffaa0177ca431662c
cometj03 <harryjung1001@gmail.com>
2024-05-23 16:18:59 +0900

    do while 구현 완료

--- src/project2/Project2.java
+++ src/project2/Project2.java
@@ -14,6 +14,7 @@ enum Token {
     DO,             // do
     WHILE,          // while
 
+    // 연산자 & 특수기호
     ASSIGN_OP,      // =
     ADD_OP,         // +
     SUB_OP,         // -
@@ -43,14 +44,9 @@ public class Project2 {
             String input = sc.nextLine();
             if (input.equals("terminate")) break;
 
-//            LexicalAnalyzer la = new LexicalAnalyzer(input);
-//            while (la.nextToken != Token.EOF && la.nextToken != Token.UNKNOWN) {
-//                la.lex();
-//                System.out.println("1. " + la.nextToken);
-//            }
-
             RDParser parser = new RDParser(input);
             try {
+                // 파싱을 먼저 시도한 후 실행합니다.
                 parser.parse();
                 parser.execute();
             } catch (SyntaxException e) {
@@ -244,12 +240,17 @@ class RDParser {
         la = new LexicalAnalyzer(input);
     }
 
+    // 입력을 재귀적으로 파싱하는 함수
+    // print, do-while 문 실행하지 않음
+    // 파싱 중 오류가 발생하면 SyntaxException을 throw 한다.
     public void parse() throws SyntaxException {
         exe = false;
+        VARMAP.clear();
         la.init();
         program();
     }
 
+    // 입력을 파싱하면서 print, do-while 문을 실행시키는 함수
     public void execute() throws SyntaxException {
         exe = true;
         VARMAP.clear();
@@ -300,7 +301,28 @@ class RDParser {
                 }
             }
             case DO -> {
-                
+                la.lex();
+                if (la.nextToken != Token.LEFT_BRACE) throw new SyntaxException();
+
+                // 블록의 시작이 되는 인덱스를 임시로 저장합니다.
+                int checkpoint = la.getCheckPoint();
+                boolean b;
+                do {
+                    la.lex();
+                    while (la.nextToken != Token.RIGHT_BRACE)
+                        statement();
+                    la.lex();
+                    if (la.nextToken != Token.WHILE) throw new SyntaxException();
+                    la.lex();
+                    if (la.nextToken != Token.LEFT_PAREN) throw new SyntaxException();
+                    la.lex();
+                    b = bexpr() && exe;
+                    if (la.nextToken != Token.RIGHT_PAREN) throw new SyntaxException();
+                    if (b) {
+                        // bexpr의 값이 true이면 블록의 시작으로 이동합니다.
+                        la.setCheckPoint(checkpoint);
+                    }
+                } while (b);
                 la.lex();
             }
             default -> throw new SyntaxException();
